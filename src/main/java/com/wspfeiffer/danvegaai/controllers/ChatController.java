fe4e69274849d82dbc0944555da2423037145ee4
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2024-09-29 11:08:55 -0400

    First working hello world with spring boot chatgpt

--- src/main/java/com/wspfeiffer/danvegaai/controllers/ChatController.java
+++ src/main/java/com/wspfeiffer/danvegaai/controllers/ChatController.java
@@ -1,4 +1,26 @@
 package com.wspfeiffer.danvegaai.controllers;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/dad-jokes")
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a data joke") String message) {
+
+        ChatClient.CallResponseSpec callResponseSpec = chatClient.prompt()
+                .user(message).call();
+        String test = callResponseSpec.content();
+
+        return test;
+    }
 }
